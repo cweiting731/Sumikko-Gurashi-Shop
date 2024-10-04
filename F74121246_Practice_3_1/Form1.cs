03fb9dcc6bacab948308eb8bf719a4b9d5097b22
willy Chou <willy931118@gmail.com>
2024-10-04 20:12:51 +0800

    Create FrontPage, SignInPage, and MainPage. Use Form1.cs to connect three page and finish FrontPage and SignInPage

---
+++ F74121246_Practice_3_1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F74121246_Practice_3_1
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1000, 700);
+            LoadFrontPage();
+        }
+
+        private void LoadFrontPage()
+        {
+            this.Controls.Clear();
+            FrontPage frontPage = new FrontPage();
+            frontPage.Dock = DockStyle.Fill;
+
+            frontPage.OnOpenShopClicked += FrontPage_OpenShopClick;
+
+            this.Controls.Add(frontPage);
+        }
+
+        private void FrontPage_OpenShopClick(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            SignInPage signInPage = new SignInPage();
+            signInPage.Dock = DockStyle.Fill;
+
+            signInPage.OnChangeToMainPage += SignInPage_ChangeToMainPage;
+
+            this.Controls.Add(signInPage);
+        }
+
+        private void SignInPage_ChangeToMainPage(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            MainPage mainPage = new MainPage();
+            mainPage.Dock = DockStyle.Fill;
+            this.Controls.Add(mainPage);
+        }
+    }
+}
---
+++ F74121246_Practice_3_1/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F74121246_Practice_3_1
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
